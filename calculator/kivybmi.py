50138dcd016f1caf933a7a681bbec2f4edb22118
nevimjaksejmenovat <mikurecka@gmail.com>
2021-03-11 23:31:33 +0100

    Vypocita tve bmi, pomoci zadanych hodnot, pokud se hodnota vymaze program spadne

--- calculator/kivybmi.py
+++ calculator/kivybmi.py
@@ -9,6 +9,7 @@ DEFAULTA = 20  #default age
 DEFAULTBMI = 21 #defult bmi
 
 class FloatLayout(FloatLayout):
+    bmi = 20
     Window.clearcolor = (255, 255, 255, 255)
     pass
 
@@ -21,10 +22,32 @@ class BmiBuild(App):
         self.age = DEFAULTA
         self.bmi = DEFAULTBMI
         return FloatLayout()
+    def result(self, finalbmi):
+        if self.age >= 11:
+            if finalbmi < 16:
+                self.root.ids.label_result_bmi.text = "Severe Thinnes"
+            elif finalbmi > 16 and finalbmi <= 17:
+                self.root.ids.label_result_bmi.text = "Moderate Thinnes"
+            elif finalbmi > 17 and finalbmi <= 18.5:
+                self.root.ids.label_result_bmi.text = "Mild Thinnes"
+            elif finalbmi > 18.5 and finalbmi <= 25:
+                self.root.ids.label_result_bmi.text = "Normal"
+            elif finalbmi > 25 and finalbmi <= 30:
+                self.root.ids.label_result_bmi.text = "Qwerweight"
+            elif finalbmi > 30 and finalbmi <= 35:
+                self.root.ids.label_result_bmi.text = "Mildly Obese"
+            elif finalbmi > 35 and finalbmi <= 40:
+                self.root.ids.label_result_bmi.text = "Obese"
+            else:
+                self.root.ids.label_result_bmi.text = "Morbidly Obese"
+
+
 
     def calculate(self, event):
         self.bmi = (self.weight / self.height / self.height)*10000
         bmifinal = round(self.bmi, 2)
+        self.root.ids.label_final_bmi.text = str(bmifinal)
+        BmiBuild.result(self, bmifinal)
         print(bmifinal)
         return bmifinal
 
@@ -51,7 +74,6 @@ class BmiBuild(App):
             self.age = float(age)
         else:
             self.age = int(DEFAULTA)
-
 
 
 root = BmiBuild()
